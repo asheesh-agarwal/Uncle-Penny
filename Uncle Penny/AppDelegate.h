698fc025d424491c883ee26ce6f1ab90f5cb51d0
Asheesh Agarwal <aa2339@cornell.edu>
2015-10-10 15:05:39 -0400

    Initial commit

---
+++ Uncle Penny/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Uncle Penny
+//
+//  Created by Asheesh Agarwal on 10/10/15.
+//  Copyright © 2015 Asheesh Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Uncle Penny/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Uncle Penny
+//
+//  Created by Asheesh Agarwal on 10/10/15.
+//  Copyright © 2015 Asheesh Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
